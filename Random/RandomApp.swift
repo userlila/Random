9e9e0ec65949bff7729ed3bad93a4d9b592a4032
Lila Portis <misslilafang@gmail.com>
2023-07-20 15:24:37 -0500

    Initial Commit

---
+++ Random/RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomApp.swift
+//  Random
+//
+//  Created by Scholar on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
